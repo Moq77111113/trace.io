f7b193281d87aa277afdafa9805cf18dbc8cbe98
Quentin Moessner <quentin.moessner@gmail.com>
2026-06-12 16:09:55 +0200

    chore: mark v0.1.0 shipped

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const REPO_URL = 'https://github.com/Moq77111113/trace';
 export const LANDING_REPO_URL = 'https://github.com/Moq77111113/trace.io';
 
-export const VERSION = '0.1.0-mvp';
+export const VERSION = '0.1.0';
 
 export const SITE_URL = 'https://moq77111113.github.io/trace.io';
 export const COMPOSE_URL = `${SITE_URL}/compose.yml`;
